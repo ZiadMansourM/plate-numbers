e0cabe4e4289f2c1f3991db55376a6d0f6ae3173
Ziad Mansour <ziadmansour.4.9.2000@gmail.com>
2023-04-29 02:36:24 +0300

    🔧 FIX: circular dependency

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ uvicorn main:app --host 0.0.0.0 --port 8000
 ```
 
 ziadh
+
+```console
+ssh -i ~/.ssh/bigdata-computervision_key.pem ziadh@20.219.159.85
+
+ziadh@bigdata-computervision:~$ ls
+ziadh@bigdata-computervision:~$ pwd
+/home/ziadh
+ziadh@bigdata-computervision:~$ 
+
+ziadh@bigdata-computervision:~$ git clone https://github.com/ZiadMansourM/plate-numbers.git
+Cloning into 'plate-numbers'...
+remote: Enumerating objects: 41, done.
+remote: Counting objects: 100% (41/41), done.
+remote: Compressing objects: 100% (35/35), done.
+remote: Total 41 (delta 3), reused 41 (delta 3), pack-reused 0
+Unpacking objects: 100% (41/41), 22.12 MiB | 15.16 MiB/s, done.
+ziadh@bigdata-computervision:~$ ls
+plate-numbers
+
+
+```
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
 from typing import Dict
 
-from plate_recognition import recognize_plates
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 app = FastAPI()
 
@@ -18,6 +19,46 @@ async def home():
 @app.get("/about")
 async def about():
     return {"message": "About page"}
+
+def recognize_plates(content):
+    import cv2
+    import numpy as np
+    import imutils
+    import easyocr
+    
+    nparr = np.frombuffer(content, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+    edged = cv2.Canny(bfilter, 30, 200)
+
+    keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(keypoints)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+    location = None
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 10, True)
+        if len(approx) == 4:
+            location = approx
+            break
+
+    mask = np.zeros(gray.shape, np.uint8)
+
+    new_image = cv2.drawContours(mask, [location], 0,255, -1)
+    new_image = cv2.bitwise_and(img, img, mask=mask)
+
+    (x,y) = np.where(mask==255)
+    (x1, y1) = (np.min(x), np.min(y))
+    (x2, y2) = (np.max(x), np.max(y))
+    cropped_image = gray[x1:x2+1, y1:y2+1]
+
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(cropped_image)
+
+    return result[0][-2]
 
 if __name__ == "__main__":
     import uvicorn
